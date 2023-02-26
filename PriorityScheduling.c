a00d34dc28b7b33cf43639491eb4588f269f19f7
saran-raj007 <k.saranraj000@gmail.com>
2023-02-26 09:26:14 +0530

    prioritysheduling

---
+++ PriorityScheduling.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int main(){
+    int num;
+    printf("Enter number of process: ");
+    scanf("%d\n",&num);
+    int arr_tim[n],b_tim[n],w_tim[n],prio[n],ta_tim[n];
+    printf("Enter the arrival time,burst time and priority for each process:\n");
+    for(int i=0;i<n;i++){
+        printf("P%d: ",i+1);
+        scanf("%d %d %d\n",&arr_tim[i],&b_tim[i],&prio[i]);
+    }
+    int mp[n];
+//     Turn Around time = Exit time – Arrival time
+// Waiting time = Turn Around time – Burst time
+int a[n],b[n],p[n]
+    for(int i=0;i<n;i++){
+        if(i==0){
+            for(int j=0;j<n;j++){
+                if(arr_tim[j]==0){
+                    mp[i]=i+1;
+                    ta_tim[i]=abs(b_tim[j]-arr_tim[j]);
+                    w_tim[i]=0;
+                    a[i]=arr_tim[j];
+                    b[i]=b_tim[j];
+                    p[i]=prio[j];
+                    prio[j]=10000;
+                }
+            }
+        }
+        else{
+            int min=1000000;
+            int index;
+
+            for(int j=0;j<n;j++){
+                if(min<prrio[j]){
+                    min=prio[j];
+                    index=j;
+                    mp[i]=j+1;
+                }
+
+            }
+            ta_tim[i]=abs(b_tim[j]-arr_tim[j]);
+            w_tim[i]=abs(ta_tim[j]-b_tim[j]);
+            a[i]=arr_tim[j];
+             b[i]=b_tim[j];
+             p[i]=prio[j];
+            prio[j]=100000;
+
+
+
+
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("P%d %d %d %d %d\n",mp[i],a[i],b[i],p[i],w_tim[i],ta_tim[i);
+    }
+
+}
